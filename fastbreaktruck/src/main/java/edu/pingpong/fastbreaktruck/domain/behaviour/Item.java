8c1d53aa7873d8c373df2e1a8980f87c67c1acbf
pau <pllinas@cifpfbmoll.eu>
2021-05-11 20:14:49 +0200

    feat: :sparkles: Add output formatter

--- fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/domain/behaviour/Item.java
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/domain/behaviour/Item.java
@@ -1,5 +1,7 @@
 package edu.pingpong.fastbreaktruck.domain.behaviour;
 
+import java.util.Formatter;
+
 public interface Item {
 
     public String name();
@@ -7,4 +9,17 @@ public interface Item {
     public Packing packing();
 
     public Float pvp();
+
+    // This method has been created with the help of @dfelta, first time ever formatting java outputs
+    default String outputFormatter() {
+        StringBuilder sb = new StringBuilder();
+        Formatter formatter = new Formatter(sb);
+
+        formatter.format("Item: %1$s, Packing: %2$s, Price: %3$s", this.name(), this.packing().envelop(),
+                this.pvp());
+
+        formatter.close();
+
+        return sb.toString();
+    }
 }
